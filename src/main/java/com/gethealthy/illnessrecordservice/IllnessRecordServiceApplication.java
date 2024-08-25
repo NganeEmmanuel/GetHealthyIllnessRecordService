162756282591704bff4be2dd72d840c550f31e87
Ngane Emmanuel <emmanuelngane06@gmail.com>
2024-08-25 01:09:34 -0700

    Inital commit

---
+++ src/main/java/com/gethealthy/illnessrecordservice/IllnessRecordServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gethealthy.illnessrecordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllnessRecordServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IllnessRecordServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gethealthy/illnessrecordservice/IllnessRecordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gethealthy.illnessrecordservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllnessRecordServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
